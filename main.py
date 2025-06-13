15fcaaca71142d88d8545594ac59895a4ea8a062
jin <111409737+Jerryfang1@users.noreply.github.com>
2025-06-13 14:55:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage, MessageEvent, TextMessage
+import os
+import json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -11,9 +13,12 @@ line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 
 # Google Sheets 授權設定
+credentials_json = json.loads(os.environ["GOOGLE_APPLICATION_CREDENTIALS_JSON"])
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_json, scope)
 client = gspread.authorize(creds)
+
+# 打開 Sheet
 sheet = client.open("金玥報價").worksheet("金價")
 
 @app.route("/webhook", methods=['POST'])
@@ -25,7 +30,7 @@ def webhook():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text in ["查詢黃金報價"]:
+    if event.message.text in ["查詢金價"]:
         user_id = event.source.user_id
         today = datetime.now().strftime("%Y/%m/%d")
         records = sheet.get_all_records()
@@ -38,3 +43,8 @@ def handle_message(event):
             msg = f"❗ 未找到 {today} 的金價報價，請稍後再試或聯繫店家。"
 
         line_bot_api.reply_message(event.reply_token, TextSendMessage(text=msg))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+
