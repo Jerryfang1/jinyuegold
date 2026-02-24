a6f9a8c04273b78c750008d5ee8939f9dfac4952
jin <111409737+Jerryfang1@users.noreply.github.com>
2026-02-24 14:49:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -400,10 +400,10 @@ def reply_gold_today(reply_token):
     print(f"[DEBUG] 最終使用日期：{used_date_str}")
 
     # 取值
-    gold_sell = int(matched.get("黃金賣出", "N/A")) - 300
-    gold_buy = int(matched.get("黃金買入", "N/A")) + 100
-    goldbar_sell = int(matched.get("黃金賣出", "N/A")) - 200
-    goldbar_buy = int(matched.get("黃金買入", "N/A")) + 400
+    gold_sell = int(matched.get("黃金賣出", "N/A")) - 400
+    #飾金賣出 -400
+    goldbar_sell = int(matched.get("黃金賣出", "N/A")) - 300
+    #條塊賣出 -300
     date_str = matched.get("日期", "")
     week_str = matched.get("星期", "")
     time_str = matched.get("時間", "")
