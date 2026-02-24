1fdff8cfb4c1d63be56f140cafe76b3dc43c8d27
jin <111409737+Jerryfang1@users.noreply.github.com>
2026-02-24 15:02:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,6 +76,7 @@ def handle_postback(event):
         reply_gold_today(event.reply_token)
         
 ########################################################################
+#鉑金回收價格
 def reply_ptrecycle_price(reply_token):
     today = datetime.now()
     today_str = today.strftime("%Y/%m/%d")
@@ -124,9 +125,6 @@ def reply_ptrecycle_price(reply_token):
     print(f"[DEBUG] 最終使用日期：{used_date_str}")
 
     # 取值
-    gold_sell = int(matched.get("黃金賣出", "N/A")) - 300
-    gold_buy = int(matched.get("黃金買入", "N/A")) + 100
-    pt_sell = int(matched.get("鉑金賣出", "N/A")) - 100
     pt_buy = int(matched.get("鉑金買入", "N/A")) + 100
     date_str = matched.get("日期", "")
     week_str = matched.get("星期", "")
@@ -141,9 +139,6 @@ def reply_ptrecycle_price(reply_token):
         .replace("{DATE}", date_str)
         .replace("{TIME}", time_str)
         .replace("{WEEKDAY}", week_str)
-        .replace("{GOLD_SELL}", str(gold_sell))
-        .replace("{GOLD_BUY}", str(gold_buy))
-        .replace("{PT_SELL}", str(pt_sell))
         .replace("{PT_BUY}", str(pt_buy))
     )
     
